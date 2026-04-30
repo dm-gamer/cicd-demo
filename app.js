94d9c57df8b21d722fa30d49515df762c906bfb7
sanjay <sanjayraj9042570775@gmail.com>
2026-04-30 17:20:27 +0530

    cicd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hi from Deployment</h1>');
+  res.send('<h1>Hi from Production</h1>');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
